c4f7813fca219ebc1273f1d4bda44a754b13c55d
atulkamble11 <raj.kambale@yahoo.com>
2020-06-03 23:11:47 +0530

    changed code as per comment

--- src/TestP/Test2git.java
+++ src/TestP/Test2git.java
@@ -5,7 +5,7 @@ public class Test2git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("this is another class");
+		System.out.println("this is another class . Changed as per comments");
 
 	}
 
